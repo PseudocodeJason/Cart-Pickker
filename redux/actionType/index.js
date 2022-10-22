2cc62c3107a8ea8e94aaead56a43b0070985cb8f
Jason <jason.tieu@Hotmail.com>
2022-10-22 00:16:46 -0400

    After API but Before Flex

--- redux/actionType/index.js
+++ redux/actionType/index.js
@@ -1,3 +1,5 @@
 export const ADD_ITEM = 'ADD_ITEM'
 export const REMOVE_ITEM = 'REMOVE_ITEM'
 export const MODIFY_ITEM = 'MODIFY_ITEM'
+export const SHOW_API = 'ADD_API'
+export const HIDE_API = 'HIDE_API'
